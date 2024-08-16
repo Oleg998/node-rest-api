fc998117f259d1a2c045aae897e3afbc3d383af9
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-08-16 12:23:15 +0300

    Update contactsServices.js

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,10 +1,10 @@
 import Contact from "../models/Contact.js";
 
-export function listContacts(filter,setting={}) {
-  return Contact.find(filter,"-createdAt -updatedAt",setting);
+export function listContacts(filter, setting = {}) {
+  return Contact.find(filter, "-createdAt -updatedAt", setting);
 }
-export function countContacts (filter){
-  return Contact.countDocuments(filter)
+export function countContacts(filter) {
+  return Contact.countDocuments(filter);
 }
 
 export function getContactById(filter) {
@@ -17,11 +17,12 @@ export function addContact(data) {
 }
 
 export function removeContact(filter) {
-  return Contact.findOneAndDelete(filter)}
-
-export  function updateContactByid(filter , data ){
-    return  Contact.findOneAndUpdate(filter , data)
+  return Contact.findOneAndDelete(filter);
 }
-export  function updateFavoriteByid(filter , data ){
-    return  Contact.findOneAndUpdate(filter , data)
+
+export function updateContactByid(filter, data) {
+  return Contact.findOneAndUpdate(filter, data);
+}
+export function updateFavoriteByid(filter, data) {
+  return Contact.findOneAndUpdate(filter, data);
 }
