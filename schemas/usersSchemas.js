eb784b0a975df9ae361c9d604a8ae766962e6873
Oleg998 <trussameto@gmail.com>
2024-09-21 23:45:46 +0300

    Update usersSchemas.js

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,8 +1,9 @@
 import Joi, { date } from "joi"; 
 
-import { emailRegepxp,subscriptionList } from "../costants/user-constants.js";
+import { emailRegepxp } from "../costants/user-constants.js";
 
 export const userSignupSchema = Joi.object({
+    event:Joi.string().required(),
     name:Joi.string().min(1).required(),
     email:Joi.string().pattern(emailRegepxp).required(), 
     birthday:Joi.string().required(),
