515828e9ee1f5ac18261263d9b9113c21fd09925
Oleg998 <trussameto@gmail.com>
2024-09-22 11:33:59 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const userSchema = new Schema ({
         type: String,
         match:emailRegepxp,
         required: [true, 'Email is required'],
-        unique: true,
+        unique: false,
       },
       subscription: {
         type: String,
